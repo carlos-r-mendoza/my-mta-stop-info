46710674ab3b0c7e926195733a4cfc49a37359af
carlos-r-mendoza <clos.mendoza@gmail.com>
2017-04-20 23:10:42 -0400

    My mta stop info app configured

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
+
+// a decorator function adds more behavior to our class from outside of the class
+// the decorator turns js class into a component
+// it must be declared immediately before the class 
+
+// the configurations line inside of a decorator are often referred to as meta data
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+  
 export class AppComponent {
-  title = 'app works!';
+  title = 'My MTA Stop Info';
 }
